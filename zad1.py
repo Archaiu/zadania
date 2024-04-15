a586bfc26a3bc220e6ad9f67540b93b0e236dac9
Klusiaaa <151560639+g-pyda@users.noreply.github.com>
2024-04-15 17:58:01 +0200

    Create zad1.py

---
+++ zad1.py
@@ -0,0 +1,6 @@
+print("Witaj w programie obliczającym liczbę miejsc zerowych funkcji kwadratowej ax^2 + bx + c.")
+a = input("Podaj współczynnik a: ")
+b = input("Podaj współczynnik b: ")
+c = input("Podaj współczynnik c: ")
+
+# tutaj wprowadź swój kod
